05e59dfe9b5aa69787d50b33b4d93aa44caaa6d0
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-01 09:06:02 +0900

    Article 모델에서 사용자 참조를 선택적으로 변경하고, ArticleTool 클래스를 추가하여 JSON 형태로 Article을 출력하는 기능을 구현했습니다.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,26 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  after_create :generate_title
+
+  def generate_title
+    response = Faraday.get(url)
+    if response.status == 301
+      update(url: response.headers['location'])
+      response = Faraday.get(url)
+    end
+    doc = Nokogiri::HTML(response.body)
+    title = doc.at('title').text
+    update(title:) if title.is_a?(String)
+  end
+end
+
+class ArticleTool < RubyLLM::Tool
+  description "아이디로 가져온 Article을 JSON 형태로 출력합니다."
+
+  param :id, desc: "Article 아이디"
+
+  def execute(id:)
+    Article.find_by(id: id).to_json
+  end
 end
