8ee3498b9fc1398bb21902fa9227bf12ba481515
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-02 10:33:44 +0900

    변수 이름을 'script'에서 'prompt'로 변경하고, 영어로 된 지침으로 수정

--- app/jobs/article_job.rb
+++ app/jobs/article_job.rb
@@ -10,17 +10,17 @@ class ArticleJob < ApplicationJob
     article = Article.find_by(id: id)
     return unless article.is_a?(Article)
 
-    script = <<~SCRIPT
+    prompt = <<~PROMPT
 제공한 링크의 본문을 주의 깊게 읽고 요약, 정리 한 내용을 한국어로 제공합니다.
 간단한 핵심 요약과 상세 요약을 제공합니다. 요약, 정리를 하기 위해 다른 사이트를 참고 해야 할 수 있습니다.
 핵심 요약은 3줄 이내로 작성합니다.
 상세 요약은 주요 내용(main_content)과 결론(conclusion)의 형식으로 작성합니다.
 출력 결과는 파싱하기 쉽도록 JSON 형태로 제목(title_ko), 핵심 요약(summary_key), 상세 요약(summary_detail) 세 항목을 출력합니다.
-    SCRIPT
+    PROMPT
 
     chat = RubyLLM.chat
-    chat.with_instructions("당신은 ruby 언어와 rubyonrails 프레임워크의 전문가입니다.")
-    response = chat.ask("#{script} #{article.url}")
+    chat.with_instructions("You are a Ruby land and RubyOnRails framework expert.")
+    response = chat.ask("#{prompt} #{article.url}")
     # logger.debug response.content
 
     # JSON 데이터 추출 및 파싱
