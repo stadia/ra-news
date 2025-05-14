eb322e19ea23b175043eb0a85e4613429288d2fa
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-14 20:04:00 +0900

    Add default value for name column in users table

--- db/migrate/20250514014717_change_baseuri_from_site.rb
+++ db/migrate/20250514014717_change_baseuri_from_site.rb
@@ -1,6 +1,6 @@
 class ChangeBaseuriFromSite < ActiveRecord::Migration[8.0]
   def change
     change_column :sites, :base_uri, :string, null: true
-    add_column :users, :name, :string, null: false
+    add_column :users, :name, :string, null: false, default: ''
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_14_070127) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
