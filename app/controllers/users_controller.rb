4ebdb5e910315c7bb1d1453f1e0b2a214accc145
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-19 22:41:31 +0900

    세션 생성 시 리디렉션 경로 수정 및 사용자 생성 후 세션 시작 기능 추가; 사용자 폼에 URL 추가

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        start_new_session_for @user
+        format.html { redirect_to new_session_path, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
