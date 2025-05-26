e331712d5d41effdc5a39b546a7c52dc6296d215
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-26 22:56:40 +0900

    파라미터 설명 업데이트 및 execute 메서드 리팩토링: format_transcript 메서드로 분리

--- app/jobs/youtube_content_tool.rb
+++ app/jobs/youtube_content_tool.rb
@@ -5,7 +5,7 @@
 class YoutubeContentTool < RubyLLM::Tool
   description "url을 통해 가져온 Youtube 콘텐츠에서 자막과 링크를 추출합니다."
 
-  param :url, desc: "URL of the Youtube (e.g., https://example.com)"
+  param :url, desc: "URL of the Youtube (e.g., https://www.youtube.com/watch?v=ErsF7_0bZnk)"
 
   #: (url String) -> String?
   def execute(url:)
@@ -13,8 +13,13 @@ class YoutubeContentTool < RubyLLM::Tool
     return unless youtube_id
 
     rc = Youtube::Transcript.get(youtube_id)
-    tsr = rc.dig("actions").first.dig("updateEngagementPanelAction", "content", "transcriptRenderer", "content", "transcriptSearchPanelRenderer", "body", "transcriptSegmentListRenderer", "initialSegments")
-    return unless tsr
+    format_transcript(rc.dig("actions"))
+  end
+
+  private
+  def format_transcript(actions)
+    tsr = actions&.first&.dig("updateEngagementPanelAction", "content", "transcriptRenderer", "content", "transcriptSearchPanelRenderer", "body", "transcriptSegmentListRenderer", "initialSegments")
+    return nil if tsr.blank?
 
     tsr.map { |it| it.dig("transcriptSegmentRenderer", "startTimeText", "simpleText").to_s + " - " + it.dig("transcriptSegmentRenderer", "snippet", "runs").map { |it| it.dig("text") }.join(" ") }.join("\n")
   end
