e6ac2832bd2645bd27e8335f8f2d947c662516b9
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-28 19:00:34 +0900

    Add RBI definitions for YoutubeSiteJob with perform_later and perform_now methods

--- app/clients/ruby_conference.rb
+++ app/clients/ruby_conference.rb
@@ -5,15 +5,15 @@
 class RubyConference < ApplicationClient
     BASE_URI = "https://raw.githubusercontent.com/ruby-conferences/ruby-conferences.github.io/refs/heads/main"
 
-    def initialize
+    def initialize #: RubyConference
       @base_uri = BASE_URI
     end
 
-    def conferences
+    def conferences #: Array<Hash>
       YAML.load(get("/_data/conferences.yml").body, permitted_classes: [ Date ])
     end
 
-    def conferences_cached
+    def conferences_cached #: Array<Hash>
       Rails.cache.fetch("ruby-conferences/_data/conferences.yml", expires_in: 1.day) do
         conferences
       end
@@ -21,7 +21,7 @@ class RubyConference < ApplicationClient
 
     private
 
-    def content_type
+    def content_type #: String
       "text/yaml"
     end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -7,6 +7,10 @@ class Site < ApplicationRecord
 
   validates :name, :client, presence: true
 
+  before_create do
+    self.last_checked_at = Time.zone.now.beginning_of_month if last_checked_at.blank?
+  end
+
   def init_client #: Object?
     return Youtube::Channel.new(id: channel) if is_youtube?
 
@@ -15,11 +19,11 @@ class Site < ApplicationRecord
     client.constantize.new(base_uri: base_uri)
   end
 
-  def is_rss?
+  def is_rss? #: bool
     client == "RssClient"
   end
 
-  def is_youtube?
+  def is_youtube? #: bool
     channel.present? && client == "Youtube::Channel"
   end
 end
