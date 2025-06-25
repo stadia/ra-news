9852bc29166d8425130549c5601d8a0c7ba9928b
jeff.dean <jeff.dean@kakaomobility.com>
2025-06-25 18:28:46 +0900

    데이터베이스 마이그레이션 수정: 'vector' 확장 기능을 활성화하는 방식으로 변경하고, 'embedding' 컬럼을 추가하여 데이터베이스 구조를 개선했습니다.

--- db/migrate/20250623100402_add_vector_body.rb
+++ db/migrate/20250623100402_add_vector_body.rb
@@ -1,6 +1,6 @@
 class AddVectorBody < ActiveRecord::Migration[8.0]
   def up
-    execute "CREATE EXTENSION IF NOT EXISTS vector;" unless Rails.env.test?
+    enable_extension "vector" unless Rails.env.test?
     execute "ALTER TABLE articles ADD COLUMN embedding vector(768);"
   end
 
