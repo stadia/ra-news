d5f4da77a6b5b28601204672ef47300c1cf94a68
jeff.dean <jeff.dean@kakaomobility.com>
2025-09-07 08:49:31 +0900

    배치잡 수정

--- app/jobs/article_batch_job.rb
+++ app/jobs/article_batch_job.rb
@@ -5,7 +5,11 @@
 class ArticleBatchJob < ApplicationJob
   queue_as :default
 
-  def perform #: void
-    ArticleBatchService.call
+  #: (?Time created_at) -> void
+  def perform(created_at = Time.zone.now.beginning_of_day)
+    Article.kept.where(title_ko: nil, created_at: created_at...).find_each do |article|
+      ArticleLlmService.call(article)
+      sleep 1
+    end
   end
 end
