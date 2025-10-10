7244e9d8d7d2ad7b555c42fd2adc33f4dbbc799c
jeff.dean <jeff.dean@kakaomobility.com>
2025-10-10 22:27:47 +0900

    Make AddUsernameToUser migration reversible

--- db/migrate/20251010124239_add_username_to_user.rb
+++ db/migrate/20251010124239_add_username_to_user.rb
@@ -1,6 +1,10 @@
 class AddUsernameToUser < ActiveRecord::Migration[8.0]
-  def change
+  def up
     add_column :users, :username, :string, null: true, limit: 50
     change_column :users, :name, :string, null: false, limit: 50
+  end
+
+  def down
+    remove_column :users, :username, :string, null: true, limit: 50
   end
 end
