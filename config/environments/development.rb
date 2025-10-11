7eed29eb81b73b84c14295bfb6abf0c28a8f8cc4
jeff.dean <jeff.dean@kakaomobility.com>
2025-10-11 21:45:14 +0900

    Enable Solid Cache with SQLite DB config
    - Add separate cache databases in development and test - Switch
    production cache from Postgres to SQLite - Set cache_store to
    :solid_cache_store - Add database key to cache.yml - Regenerate cache
    schema (Rails 8.0)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
-  # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # Use Solid Cache with SQLite3 for development.
+  config.cache_store = :solid_cache_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
