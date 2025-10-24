51141024ef557970066277fb666987fef24a3912
jeff.dean <jeff.dean@kakaomobility.com>
2025-10-24 23:38:59 +0900

    깔끔하게

--- db/migrate/20251024000000_remove_duplicate_indexes.rb
+++ db/migrate/20251024000000_remove_duplicate_indexes.rb
@@ -2,8 +2,6 @@
 
 class RemoveDuplicateIndexes < ActiveRecord::Migration[8.0]
   def up
-    remove_index :sites, name: :index_sites_on_client_and_last_checked
-
     remove_index :sites, name: :index_sites_on_client_and_last_checked_at
 
     remove_index :sites, name: :index_sites_on_last_checked_at
@@ -11,8 +9,6 @@ class RemoveDuplicateIndexes < ActiveRecord::Migration[8.0]
     remove_index :taggings, name: :index_taggings_on_context
 
     remove_index :taggings, name: :index_taggings_on_tagger_type_and_tagger_id
-
-    remove_index :comments, name: :index_comments_on_article_and_created_at
   end
 
   def down
