a69fc902dd501f791a923d43523447aa45411bcf
jeff.dean <jeff.dean@kakaomobility.com>
2025-10-27 22:15:36 +0900

    Configure Federails actor types on initialization
    The User class needs to be loaded during Rails initialization so that
    acts_as_federails_actor automatically registers it with Federails.

--- config/initializers/federails.rb
+++ config/initializers/federails.rb
@@ -1,1 +1,7 @@
 Federails.config_from "federails"
+
+# User 클래스를 로드하여 Federails::Configuration.actor_types를 설정
+# acts_as_federails_actor 호출 시 자동으로 register_actor_class가 실행됨
+Rails.application.config.after_initialize do
+  User # User 클래스를 참조하여 로드
+end
