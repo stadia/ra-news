78ff737aa51425949820fe36871813e3c3c71059
jeff.dean <jeff.dean@kakaomobility.com>
2026-02-06 16:29:08 +0900

    Tidify comment and article markup and modal data

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,26 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = []
+  static targets = [];
 
   connect() {
-    this.modalId = this.element.dataset.modalTarget
-    this.modal = document.getElementById(this.modalId)
+    this.modalId = this.element.dataset.modalId;
+    this.modal = document.getElementById(this.modalId);
   }
 
   open() {
     if (this.modal) {
-      this.modal.classList.remove("hidden")
+      this.modal.classList.remove("hidden");
       // Prevent body scrolling when modal is open
-      document.body.style.overflow = "hidden"
+      document.body.style.overflow = "hidden";
     }
   }
 
   close() {
     if (this.modal) {
-      this.modal.classList.add("hidden")
+      this.modal.classList.add("hidden");
       // Restore body scrolling
-      document.body.style.overflow = ""
+      document.body.style.overflow = "";
     }
   }
 }
