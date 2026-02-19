ae4c785961337e6bca68dbc15f00e0a46b2b80c0
jeff.dean <jeff.dean@kakaomobility.com>
2026-02-20 00:01:28 +0900

    Increase article body length threshold and remove URL links
    - Change minimum body length from 25 to 30 characters
    - Add validation to discard articles with body length under 31 chars
    - Remove link_to wrapper
    
    # Conflicts:
    #	app/views/articles/index.html.erb
    #	app/views/articles/others.html.erb

--- app/services/article_agents_service.rb
+++ app/services/article_agents_service.rb
@@ -13,10 +13,10 @@ class ArticleAgentsService < OperationService
   private
 
   def ensure_body(article)
-    return Success(article) if article.body.present? && article.body.size >= 25
+    return Success(article) if article.body.present? && article.body.size > 30
 
     body_result = ContentService.new.call(article)
-    if body_result.failure?
+    if body_result.failure? || body_result.value!.size < 31
       article.discard!
       return Failure(body_result.failure)
     end
