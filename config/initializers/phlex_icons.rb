dfc05f4c3c7025f9db211e8e73fcb9d8f6b1b4e4
jeff.dean <jeff.dean@kakaomobility.com>
2026-02-19 13:50:24 +0900

    Update youtube-transcript-rb to 0.2.4 and tweak icon config

--- config/initializers/phlex_icons.rb
+++ config/initializers/phlex_icons.rb
@@ -1,5 +1,5 @@
 PhlexIcons.configure do |config|
   config.default_variant = :solid
-  config.helper_method_name = :phlex_icon # Default: :phlex_icon
+  # config.helper_method_name = :phlex_icon # Default: :phlex_icon
   config.default_pack = :hero # Default: nil. Accepts :symbol, "string", or Class (e.g., PhlexIcons::Hero)
 end
