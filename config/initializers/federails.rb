fa4a8499d5509ec9a95bf979771ae76757e0224b
jeff.dean <jeff.dean@kakaomobility.com>
2026-03-09 15:43:00 +0900

    Add federation delivery debug logging to Federails
    Prepend logging module to `Fediverse::Notifier` to trace activity
    delivery: logs actor info, addressing fields, resolved inboxes, and
    per-inbox delivery results/errors.

--- config/initializers/federails.rb
+++ config/initializers/federails.rb
@@ -10,4 +10,35 @@ Rails.application.config.after_initialize do
       nil
     end
   end
+
+  # Federation delivery 디버깅 로깅
+  Fediverse::Notifier.singleton_class.prepend(Module.new do
+    def post_to_inboxes(activity)
+      Rails.logger.info { "[Federation] post_to_inboxes called for Activity##{activity.id} (#{activity.action} #{activity.entity_type}##{activity.entity_id})" }
+      Rails.logger.info { "[Federation] Actor: #{activity.actor.username} (local=#{activity.actor.local?})" }
+      Rails.logger.info { "[Federation] Addressing - to: #{activity.to.inspect}, cc: #{activity.cc.inspect}" }
+
+      inboxes = send(:inboxes_for, activity)
+      Rails.logger.info { "[Federation] Resolved inboxes: #{inboxes.inspect}" }
+
+      if inboxes.none?
+        Rails.logger.warn { "[Federation] No inboxes found - activity will not be delivered" }
+        return
+      end
+
+      super
+    end
+
+    private
+
+    def post_to_inbox(inbox_url:, message:, from: nil)
+      Rails.logger.info { "[Federation] Delivering to inbox: #{inbox_url}" }
+      result = super
+      Rails.logger.info { "[Federation] Delivery result to #{inbox_url}: #{result&.status}" }
+      result
+    rescue => e
+      Rails.logger.error { "[Federation] Delivery FAILED to #{inbox_url}: #{e.class} - #{e.message}" }
+      raise
+    end
+  end)
 end
