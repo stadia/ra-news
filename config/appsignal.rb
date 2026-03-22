2381872d444eb149116d4ddd8b9b88bb28b57818
jeff.dean <stadia@gmail.com>
2026-03-22 23:07:04 +0900

    Disable AppSignal in development and remove hardcoded API key

--- config/appsignal.rb
+++ config/appsignal.rb
@@ -2,13 +2,8 @@
 # Visit our documentation for a list of all available configuration options.
 # https://docs.appsignal.com/ruby/configuration/options.html
 Appsignal.configure do |config|
-  config.activate_if_environment("development", "production")
+  config.activate_if_environment("production")
   config.name = "ruby-news.kr"
-  # The application's Push API key
-  # We recommend removing this line and setting this option with the
-  # APPSIGNAL_PUSH_API_KEY environment variable instead.
-  # https://docs.appsignal.com/ruby/configuration/options.html#option-push_api_key
-  config.push_api_key = "b5c5d63d-3fdd-400b-b47a-711d8b8eee46"
 
   # Configure actions that should not be monitored by AppSignal.
   # For more information see our docs:
