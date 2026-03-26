56f29be95f7a75f2c20a9c32e37fcebe3e8a5b4f
jeff.dean <jeff.dean@kakaomobility.com>
2026-03-26 11:38:00 +0900

    Enable cache store sessions and 1-week remember me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
+  config.session_store :cache_store, expire_after: 1.weeks
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
