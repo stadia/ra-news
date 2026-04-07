615964e01ebeaad89a30304825d5b474de592910
jeff.dean <jeff.dean@kakaomobility.com>
2026-04-07 15:16:09 +0900

    Move LetterOpener config to development environment and update Korean Devise translations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,3 +86,16 @@ Rails.application.configure do
     Prosopite.rails_logger = true
   end
 end
+LetterOpener.configure do |config|
+  # To overrider the location for message storage.
+  # Default value is `tmp/letter_opener`
+
+  # To render only the message body, without any metadata or extra containers or styling.
+  # Default value is `:default` that renders styled message with showing useful metadata.
+  config.message_template = :light
+
+  # To change default file URI scheme you can provide `file_uri_scheme` config.
+  # It might be useful when you use WSL (Windows Subsystem for Linux) and default
+  # scheme doesn't work for you.
+  # Default value is blank
+end
