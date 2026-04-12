ab3a0f5954803f73930f6f2358fd198339e6dd5c
jeff.dean <stadia@gmail.com>
2026-04-12 15:48:39 +0900

    갯수를 좀 더 적게

--- app/jobs/social_post_job.rb
+++ app/jobs/social_post_job.rb
@@ -13,7 +13,7 @@ class SocialPostJob < ApplicationJob
 
     scope = Article.kept
     scope = if id.nil?
-      scope.confirmed.where("is_posted = ?", false).where(created_at: created_at..).limit(5)
+      scope.confirmed.where("is_posted = ?", false).where(created_at: created_at..).limit(4)
     else
       scope.where("id = ? AND is_posted = ?", id, false)
     end
