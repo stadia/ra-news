4de4d218ca80c2cd5d479f2c7a11ad16adac7aa6
jeff.dean <stadia@gmail.com>
2026-04-13 14:09:54 +0900

    Add Slack OAuth link to footer navigation

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -331,6 +331,7 @@ class Components::Layout < Components::Base
         ul(class: "flex flex-wrap items-center mt-3 text-sm font-medium text-content-secondary sm:mt-0 gap-4") do
           li { render_mastodon_link }
           li { render_twitter_link }
+          li { render_slack_link }
           li { render_rss_footer_link }
         end
       end
@@ -378,6 +379,22 @@ class Components::Layout < Components::Base
     ) do
       render PhlexIcons::Hero::Rss.new(variant: :outline, class: "w-5 h-5")
       plain " RSS 피드"
+    end
+  end
+
+  def render_slack_link
+    a(
+      href: "https://slack.com/oauth/v2/authorize?client_id=8355153845137.10893405283266&scope=incoming-webhook&user_scope=",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      class: "hover:underline hover:text-content flex items-center gap-1"
+    ) do
+      svg(class: "w-5 h-5", fill: "currentColor", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg") do |s|
+        s.path(
+          d: "M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zm1.271 0a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zm0 1.271a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zm-1.27 0a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.163 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.163 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.163 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zm0-1.27a2.527 2.527 0 0 1-2.52-2.523 2.527 2.527 0 0 1 2.52-2.52h6.315A2.528 2.528 0 0 1 24 15.163a2.528 2.528 0 0 1-2.522 2.523h-6.315z"
+        )
+      end
+      plain " Slack 추가"
     end
   end
 end
