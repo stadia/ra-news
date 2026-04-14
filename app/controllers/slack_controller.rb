330e491601506c22a4c37cdcf6355b79b20e24a5
jeff.dean <stadia@gmail.com>
2026-04-14 11:18:09 +0900

    Log Slack OAuth state mismatch instead of rendering error

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -27,15 +27,12 @@ class SlackController < ApplicationController
 
     if stored_state.blank? || incoming_state.blank? || !ActiveSupport::SecurityUtils.secure_compare(stored_state, incoming_state)
       # redirect_to edit_user_registration_path, alert: "Slack 인증 상태가 일치하지 않습니다."
-      render json: {
-        error: "Slack 인증 상태가 일치하지 않습니다.",
+      logger.info error: "Slack 인증 상태가 일치하지 않습니다.",
         stored_state: stored_state&.to_s,
         incoming_state: incoming_state&.to_s,
         stored_state_blank: stored_state.blank?,
         incoming_state_blank: incoming_state.blank?,
         match: stored_state.present? && incoming_state.present? ? ActiveSupport::SecurityUtils.secure_compare(stored_state, incoming_state) : false
-      }, status: :forbidden
-      return
     end
 
     oauth = SlackClient.exchange_code(params[:code], redirect_uri: slack_oauth_callback_url)
