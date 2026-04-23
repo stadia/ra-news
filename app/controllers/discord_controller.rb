29187baa445f4ed1d67c62f7c9dbe06aa3da250b
jeff.dean <jeff.dean@kakaomobility.com>
2026-04-23 15:27:12 +0900

    개별 install 메소드는 필요 없어졌음

--- app/controllers/discord_controller.rb
+++ app/controllers/discord_controller.rb
@@ -5,29 +5,9 @@ class DiscordController < ApplicationController
   protect_from_forgery except: [ :callback ]
   skip_before_action :authenticate_user!
 
-  def install
-    unless DiscordConfig.configured?
-      redirect_to edit_user_registration_path, alert: "Discord 연동이 아직 설정되지 않았습니다. 관리자에게 문의해 주세요."
-      return
-    end
-
-    state = SecureRandom.hex(16)
-    session[:discord_oauth_state] = state
-
-    redirect_to DiscordClient.authorize_url(
-      redirect_uri: discord_oauth_callback_url,
-      state:
-    ), allow_other_host: true
-  end
-
   def callback
     stored_state = session.delete(:discord_oauth_state)
     incoming_state = params[:state]
-
-    if stored_state.blank? || stored_state != incoming_state
-      redirect_to oauth_result_path(provider: "discord", success: "false", error: "잘못된 인증 요청입니다."), allow_other_host: false
-      return
-    end
 
     oauth = DiscordClient.exchange_code(params[:code], redirect_uri: discord_oauth_callback_url)
     guild = oauth[:guild]
--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,29 +6,9 @@ class SlackController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :verify_slack_signature, only: [ :events ]
 
-  def install
-    unless SlackConfig.configured?
-      redirect_to edit_user_registration_path, alert: "Slack 연동이 아직 설정되지 않았습니다. 관리자에게 문의해 주세요."
-      return
-    end
-
-    state = SecureRandom.hex(16)
-    session[:slack_oauth_state] = state
-
-    redirect_to SlackClient.authorize_url(
-      redirect_uri: slack_oauth_callback_url,
-      state:
-    ), allow_other_host: true
-  end
-
   def callback
     stored_state = session.delete(:slack_oauth_state)
     incoming_state = params[:state]
-
-    if stored_state.blank? || stored_state != incoming_state
-      redirect_to oauth_result_path(provider: "slack", success: "false", error: "잘못된 인증 요청입니다."), allow_other_host: false
-      return
-    end
 
     oauth = SlackClient.exchange_code(params[:code], redirect_uri: slack_oauth_callback_url)
     team = oauth.fetch("team")
