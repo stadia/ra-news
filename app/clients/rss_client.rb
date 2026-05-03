633de064e5fc28b742e7b7db1498e99bafce3b88
jeff.dean <stadia@gmail.com>
2026-05-03 10:02:23 +0900

    fix: RSS 파싱 시 누락된 XML 네임스페이스 자동 보정
    content:encoded, dc:creator 등 접두어가 선언되지 않은 피드에서
    NotWellFormedError 발생을 회피한다.
    
    - RssClient.parse: 1차로 표준 파싱 시도, 실패 시 repair_xml로 보정
    - repair_xml: Nokogiri로 에러에서 누락 접두어를 추출해 루트에 xmlns 선언
    - content, dc, atom, media 등 자주 누락되는 10개 NS 사전 정의
    - 알 수 없는 접두어도 unknown.namespace/* 로 폴백 처리

--- app/clients/rss_client.rb
+++ app/clients/rss_client.rb
@@ -2,9 +2,25 @@
 # rbs_inline: enabled
 
 require "rss"
+require "nokogiri"
 
 module RssClient
   module_function
+
+  # RSS/Atom 피드에서 자주 누락되는 XML 네임스페이스
+  KNOWN_NAMESPACES = {
+    "content" => "http://purl.org/rss/1.0/modules/content/",
+    "dc" => "http://purl.org/dc/elements/1.1/",
+    "atom" => "http://www.w3.org/2005/Atom",
+    "media" => "http://search.yahoo.com/mrss/",
+    "slash" => "http://purl.org/rss/1.0/modules/slash/",
+    "wfw" => "http://wellformedweb.org/CommentAPI/",
+    "feedburner" => "http://rssnamespace.org/feedburner/ext/1.0",
+    "sy" => "http://purl.org/rss/1.0/modules/syndication/",
+    "admin" => "http://webns.net/mvcb/",
+    "rdf" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  }.freeze
+  private_constant :KNOWN_NAMESPACES
 
   #: (String url) -> RSS::Rss | RSS::Atom::Feed | nil
   def feed(url)
@@ -12,6 +28,30 @@ module RssClient
     if response.status.between?(300, 399) && response.headers["location"]
       response = Faraday.get(response.headers["location"])
     end
-    RSS::Parser.parse(response.body, false)
+    parse(response.body)
+  end
+
+  #: (String xml) -> RSS::Rss | RSS::Atom::Feed | nil
+  def parse(xml)
+    RSS::Parser.parse(xml, false)
+  rescue RSS::NotWellFormedError
+    RSS::Parser.parse(repair_xml(xml), false)
+  end
+
+  #: (String xml) -> String
+  def repair_xml(xml)
+    doc = Nokogiri::XML(xml)
+    root = doc.at("rss") || doc.at("feed") || doc.root
+    return xml unless root
+
+    doc.errors.each do |error|
+      if error.to_s =~ /Namespace prefix (\w+) on/i
+        prefix = $1
+        uri = KNOWN_NAMESPACES[prefix] || "http://unknown.namespace/#{prefix}"
+        root["xmlns:#{prefix}"] = uri
+      end
+    end
+
+    doc.to_xml
   end
 end
