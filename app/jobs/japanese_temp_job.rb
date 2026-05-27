c5a2f6a2e0f9193ccc2bfb919e60f2fce625f841
jeff.dean <jeff.dean@kakaomobility.com>
2026-05-27 12:56:47 +0900

    아이디와 슬러그

--- app/jobs/japanese_temp_job.rb
+++ app/jobs/japanese_temp_job.rb
@@ -1,5 +1,12 @@
 class JapaneseTempJob < ApplicationJob
-  def perform
+  def perform(id: nil, slug: nil)
+    if id.present? || slug.present?
+      article = Article.find_by(id:)
+      article = Article.find_by(slug:) if article.nil?
+      ArticleAgentsService.new.send(:run_japanese, article)
+      return
+    end
+
     Article.kept.confirmed.where(title_ja: nil).order("created_at desc").limit(2).each do |article|
       ArticleAgentsService.new.send(:run_japanese, article)
       sleep 5
