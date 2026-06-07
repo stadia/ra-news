9ba0e587cc5f12000d03fc19448d57a2644bb2c1
jeff.dean <stadia@gmail.com>
2026-06-07 20:07:23 +0900

    rubocop

--- test/system/boosts_test.rb
+++ test/system/boosts_test.rb
@@ -13,11 +13,13 @@ class BoostsTest < ApplicationSystemTestCase
     visit article_path(@article)
 
     frame = "#boost_article_#{@article.id}"
+
     assert_selector "#{frame} form", wait: 5
     assert_equal 0, @article.reload.boosters_count
 
     # 1) 부스트 — turbo_stream 으로 버튼이 교체되며 카운트 1 표시
     find("#{frame} button[type=submit]").click
+
     assert_selector frame, text: "1", wait: 5
     assert @user.boosts?(@article), "Boost row should be persisted after click"
     assert_equal 1, @article.reload.boosters_count
@@ -26,6 +28,7 @@ class BoostsTest < ApplicationSystemTestCase
 
     # 2) 부스트 취소 — 카운트 사라지고 행 삭제
     find("#{frame} button[type=submit]").click
+
     assert_no_selector frame, text: "1", wait: 5
     assert_not @user.boosts?(@article), "Boost row should be removed after second click"
     assert_equal 0, @article.reload.boosters_count
