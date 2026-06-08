96921ec386d1039661b52adafedb9572e89970ef
jeff.dean <stadia@gmail.com>
2026-06-08 18:21:27 +0900

    Add Sentry error tracking
    Configure Sentry with a Rails initializer and add the
    `sentry-rails` and `sentry-ruby` gems alongside existing
    monitoring dependencies.

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
